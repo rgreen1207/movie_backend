e28b72cb9b86dcb7d9c60d7e561eeb01193064e4
Ryan Green <ryan@rgreen.dev>
2022-12-02 16:39:20 -0800

    updating readme and init

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.getcwd()}/movie_flask.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 db = SQLAlchemy(app)
 
 
